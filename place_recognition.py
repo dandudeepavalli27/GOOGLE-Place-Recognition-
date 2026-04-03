3bd3035b24bb9c42c58abbbeff19873b06dcfad7
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:26:46 +0530

    Create place_recognition.py

---
+++ place_recognition.py
@@ -0,0 +1,13 @@
+# SESSION 12 – Place Recognition
+
+# Step 1: Input values
+similarity = 0.94
+threshold = 0.9
+
+# Step 2: Apply condition
+if similarity > threshold:
+    print("Place Recognized")
+else:
+    print("Place Not Recognized")
+
+print("\nProgram Executed Successfully")
